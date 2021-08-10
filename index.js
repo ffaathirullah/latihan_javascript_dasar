d717e4c0e2c4f8106259840f7849b5ca1fdc87e9
Fachrul_Faathirullah <lenpuchongloi93@gmail.com>
2021-08-11 02:42:48 +0700

    rekursif

--- index.js
+++ index.js
@@ -1,8 +1,14 @@
-function tampilAngka(n) {
-  if (n === 0) {
-    return;
-  }
-  console.log(n);
-  return tampilAngka(n - 1);
+// function tampilAngka(n) {
+//   if (n === 0) {
+//     return;
+//   }
+//   console.log(n);
+//   return tampilAngka(n - 1);
+// }
+// tampilAngka(10);
+
+function faktorial(n) {
+  if (n === 0) return 1;
+  return n * faktorial(n - 1);
 }
-tampilAngka(10);
+console.log(faktorial(5));
