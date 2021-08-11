206a4bbfa43501172e7d52fe6d573796fd9bc722
Fachrul_Faathirullah <lenpuchongloi93@gmail.com>
2021-08-11 20:02:08 +0700

    foreach

--- index.js
+++ index.js
@@ -14,9 +14,22 @@
 // console.log(faktorial(5));
 //splice(indexAwal , mauDihapusBerapa)
 
-var arr = ["fachrul", "galih", "nova", "agus", "asep"];
+// var arr = ["fachrul", "galih", "nova", "agus", "asep"];
 // arr.splice(2, 0, "doddy");
 // console.log(arr.join(" - "));
 
-var arr2 = arr.slice(1, 3);
-console.log(arr2.join("-"));
+// var arr2 = arr.slice(1, 3);
+// console.log(arr2.join("-"));
+
+var angka = [1, 2, 3, 4, 5, 6, 7, 8];
+var nama = ["fachrul", "asep", "udin"];
+// var cetak = function (e) {
+//   console.log();
+// };
+// angka.forEach((e) => {
+//   console.log(e);
+// });
+
+nama.forEach((e, i) => {
+  console.log(`mahasiswa ke ${i} adalah ${e}`);
+});
