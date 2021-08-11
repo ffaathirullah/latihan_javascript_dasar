325cae2d88d18206a09230a105d5cab36f338fda
Fachrul_Faathirullah <lenpuchongloi93@gmail.com>
2021-08-11 19:47:27 +0700

    SLICE & SPLICE

--- index.js
+++ index.js
@@ -7,8 +7,13 @@
 // }
 // tampilAngka(10);
 
-function faktorial(n) {
-  if (n === 0) return 1;
-  return n * faktorial(n - 1);
-}
-console.log(faktorial(5));
+// function faktorial(n) {
+//   if (n === 0) return 1;
+//   return n * faktorial(n - 1);
+// }
+// console.log(faktorial(5));
+//splice(indexAwal , mauDihapusBerapa)
+
+var arr = ["fachrul", "galih", "nova"];
+arr.splice(2, 0, "doddy");
+console.log(arr.join(" - "));
